a6b4acb260149321a1478100d9de242ba74afd6f
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-08-24 12:45:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import requests
+
+st.title("TalentScout")
+
+api_key = st.secrets["google"]["api_key"]
+cx = st.secrets["google"]["cx"]
+queryx = st.text_input("Search Query",placeholder="Python Developer Bengaluru").strip()
+query = f'site:linkedin.com/in "{queryx}"'
+
+if st.button("Search"):
+    if not api_key or not cx or not query:
+        st.error("Please provide API Key, Search Engine ID, and Query")
+    else:
+        query = f'site:linkedin.com/in "{query}"'
+        
+        st.write("Searching for:", queryx)
+
+        all_results = []
+        for start in range(1, 101, 10): 
+            params = {"key": api_key, "cx": cx, "q": query , "num": 10, "start": start}
+            response = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
+            
+            if response.status_code != 200:
+                st.error(f"Error: {response.status_code} - {response.text}")
+                break
+            
+            items = response.json().get("items", [])
+            if not items:
+                break  # No more results
+            all_results.extend(items)
+
+        if not all_results:
+            st.warning("No results found")
+        else:
+            st.success(f"Found {len(all_results)} results for: {query}")
+            for i, item in enumerate(all_results, start=1):
+                st.subheader(f"{i}. {item.get('title')}")
+                st.write(item.get("link"))
+                snippet = item.get("snippet", "")
+                if snippet:
+                    st.write(snippet)
